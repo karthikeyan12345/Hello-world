1ef89fd5daa290c29fcff870e89e9a07baf4a204
Karthikeyan G <41794262+karthikeyan12345@users.noreply.github.com>
2022-05-16 15:10:18 +0530

    Update 525. Contiguous Array.java

--- 525. Contiguous Array.java
+++ 525. Contiguous Array.java
@@ -21,3 +21,40 @@ class Solution {
         
     }
 }
+
+// to retur the largest contiguous subarray with equal 0 & 1.
+/*
+class Solution
+{
+	public static List<Integer> findLargestSubarray(List<Integer> nums)
+	{
+		List<Integer> largestSubarray = new ArrayList<>();
+		
+        int max=0;
+        int sum=0;
+        int left=0;
+        int right=-1;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=(nums.get(i)==0)?-1:1;
+            if(map.containsKey(sum))
+            {
+                if(max<(i-map.get(sum)))
+                {
+                	right=i;
+                	left=map.get(sum)+1;
+                    max=i-map.get(sum);
+                }
+            }
+            map.putIfAbsent(sum,i);
+        }
+        for(int i=left;i<=right;i++)
+        {
+        	largestSubarray.add(nums.get(i));
+        }
+		return largestSubarray;
+	}
+}
+*/
